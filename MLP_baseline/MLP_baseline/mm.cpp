1588bbf163e046dbdbe14ffebec60087280da59a
Sean Rice <seanrice5843@gmail.com>
2026-04-04 11:15:13 -0700

    Initial commit

---
+++ MLP_baseline/MLP_baseline/mm.cpp
@@ -0,0 +1,5 @@
+#include "mm.h"
+
+void MM(DTYPE* A, DTYPE* B, DTYPE* C, DTYPE* ABC, int N, int M, int P) {
+    //  Write your code here for blocked matrix multiplication
+}
---
+++ MLP_baseline/MLP_baseline/mm_test.cpp
@@ -0,0 +1,128 @@
+#include "mm.h"
+
+// ABC[N*P] = A[N*M] . B[M*P] + C[N]
+void mm_sw(DTYPE* A, DTYPE* B, DTYPE* C, DTYPE* ABC, int N, int M, int P) {
+    //write your code here for three loops of computing ABC[N*P]
+}
+
+void readWeightsAndBiases(const string& wfilename, DTYPE* weight, DTYPE* bias, int H, int W) {
+    cout << "\tReading Weights and Biases from file: " << wfilename << endl;
+    ifstream wFile(wfilename);
+    if (!wFile.is_open()) {
+        cerr << "\t\tError opening file: " << wfilename << endl;
+    }
+    if (wFile.eof()) {
+        cerr << "\t\tError: Reached end of file unexpectedly!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char ch;
+    char buffer[50];
+    int weightIndex = 0;
+    int biasIndex = 0;
+    int bufIndex = 0;
+    bool readingWeights = false;
+    bool readingBiases = false;
+    bool isNegative = false;
+
+    while (wFile.get(ch)) {
+        if (ch == 'W' && wFile.peek() == 'e') {
+            readingWeights = true;
+            readingBiases = false;
+            wFile.ignore(100, ']');
+        } 
+        else if (ch == 'b' && wFile.peek() == 'i') {
+            readingWeights = false;
+            readingBiases = true;
+            wFile.ignore(100, ']');
+        }
+
+        if ((isdigit(ch) || ch == '.' || ch == '-') && bufIndex < 50) {
+            if (ch == '-') {
+                isNegative = true;
+            } 
+            else {
+                buffer[bufIndex++] = ch;
+            }
+        } 
+        else if (bufIndex > 0) {
+            buffer[bufIndex] = '\0';
+            float value = atof(buffer);
+            if (isNegative) 
+                value = -value;
+            
+            if (readingWeights && weightIndex < H*W) {
+                weight[weightIndex++] = value;
+            } 
+            else if (readingBiases && biasIndex < H) {
+                bias[biasIndex++] = value;
+            }
+            
+            bufIndex = 0;
+            isNegative = false;
+        }
+    }
+    cout << "number of read values for weights: " << weightIndex << endl;
+    cout << "number of read values for biases: " << biasIndex << endl;
+    wFile.close();
+}
+
+void readImages(const string& imgfilename, DTYPE* data) {
+    cout << "\tReading input images from file: " << imgfilename << endl;
+    ifstream imgFile(imgfilename);
+    if (!imgFile.is_open()) {
+        cerr << "\t\tError opening file: " << imgfilename << endl;
+    }
+    if (imgFile.eof()) {
+        cerr << "\t\tError: Reached end of file unexpectedly!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int value;
+    for (int j = 0; j < BATCH_SIZE; j++) {
+        for (int i = 0; i < IMAGE_SIZE; i++) {
+            imgFile >> value;
+            data[i*BATCH_SIZE+j] = value / 256;
+        }
+    }
+    
+    imgFile.close();
+}
+
+const int N = 1024;
+const int M = 784;
+const int P = 128;
+
+int main()
+{
+	DTYPE* y1 = new DTYPE[N*P];
+    DTYPE* w1 = new DTYPE[N*M];
+    DTYPE* in = new DTYPE[M*P];
+    DTYPE* b1 = new DTYPE[N];
+
+	cout << "\n---Testing results----------------------------------\n";
+
+    readImages("mnist_images_int.txt", in);
+    readWeightsAndBiases("weights_layer1.txt", w1, b1, 1024, IMAGE_SIZE);
+
+    // Instantiate your function here
+	
+    
+    ofstream oFile("first_layer_actual_output.txt");
+	for (int i = 0; i < N*P; i++)
+		oFile << y1[i] << endl;
+    oFile.close();
+
+    cout << "Comparing against output data " << endl;
+    if (system("diff -w first_layer_actual_output.txt first_layer_golden_output.txt")) {
+        cout << "*******************************************" << endl;
+        cout << "FAIL: Output DOES NOT match the golden output" << endl;
+        cout << "*******************************************" << endl;
+        return 1;
+    } else {
+        cout << "*******************************************" << endl;
+        cout << "PASS: The output matches the golden output!" << endl;
+        cout << "*******************************************" << endl;
+        return 0;
+    }
+}
